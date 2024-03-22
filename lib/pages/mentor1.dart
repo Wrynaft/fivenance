443ebf6519c828903d55cd90a757b6abfb9ed141
Wrynaft <wrynaft@gmail.com>
2024-03-22 18:24:07 +0800

    added const at mentor1

--- lib/pages/mentor1.dart
+++ lib/pages/mentor1.dart
@@ -15,7 +15,7 @@ class _Mentor1State extends State<Mentor1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF0C1C3C),
+      backgroundColor: const Color(0xFF0C1C3C),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
@@ -25,24 +25,24 @@ class _Mentor1State extends State<Mentor1> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                Column(children: <Widget>[
+                const Column(children: <Widget>[
                   Text('Mentorship', style: TextStyle(color: Colors.white, fontSize: 40)),
                   Text('Sign Up', style: TextStyle(color: Color(0xFFCACACA), fontSize: 40)),
                 ],),
                 Container(
-                margin: EdgeInsets.all(40),
+                margin: const EdgeInsets.all(40),
                 alignment: Alignment.center,
-                child: Image(
+                child: const Image(
                   width: 100,
                   image: AssetImage('assets/Logo.png'),
                 ),),
               ],),
 
             Container(
-              margin:EdgeInsets.only(left:20, right:20, bottom: 20),
-              child: LinearProgressIndicator(value: 0.33, color: Color(0xFF64FF8C), backgroundColor: Color(0xFF523F62),)),
+              margin:const EdgeInsets.only(left:20, right:20, bottom: 20),
+              child: const LinearProgressIndicator(value: 0.33, color: Color(0xFF64FF8C), backgroundColor: Color(0xFF523F62),)),
 
-            Center(child: Text('Page 1: Personal Details', style: TextStyle(color: Colors.white, fontSize: 26))),
+            const Center(child: Text('Page 1: Personal Details', style: TextStyle(color: Colors.white, fontSize: 26))),
             detailsTextField('Email', false),
             detailsTextField('Phone Number', false),
             Row(
@@ -55,7 +55,7 @@ class _Mentor1State extends State<Mentor1> {
             detailsTextField('Job Title', false),
             detailsTextField('Bank Account No.', false),
             Container(
-              margin: EdgeInsets.only(top: 20),
+              margin: const EdgeInsets.only(top: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
@@ -64,7 +64,7 @@ class _Mentor1State extends State<Mentor1> {
                   style: OutlinedButton.styleFrom(side: const BorderSide(color: Color(0xFF6DFF95))),
                   child: const Text('Cancel', style: TextStyle(fontFamily: 'Judson', color: Color(0xFF6DFF95), fontSize: 16)),),
               
-                ElevatedButton(
+                const ElevatedButton(
                   style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color(0xFF55E985))),
                   onPressed: null, child: Text('Next', style: TextStyle(color: Colors.black, fontSize: 16)))
               ],),
@@ -77,23 +77,23 @@ class _Mentor1State extends State<Mentor1> {
 
   Container detailsTextField(String label, bool obscure) {
     return Container(
-        margin: EdgeInsets.only(left: 30, right:30, top:20),
+        margin: const EdgeInsets.only(left: 30, right:30, top:20),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
-          gradient: LinearGradient(colors: [Color(0xFF10FFA9).withOpacity(0.2), Color(0XFFFFFFFF).withOpacity(0.2)]),
+          gradient: LinearGradient(colors: [const Color(0xFF10FFA9).withOpacity(0.2), const Color(0XFFFFFFFF).withOpacity(0.2)]),
         ),
         child: SizedBox(
           width: 250,
           child: TextField(
             obscureText: obscure,
-            style: TextStyle(color: Colors.white, fontSize: 18),
+            style: const TextStyle(color: Colors.white, fontSize: 18),
           decoration: InputDecoration(
             border: OutlineInputBorder(
               borderSide: BorderSide.none,
               borderRadius: BorderRadius.circular(20)
             ),
             labelText: label,
-            labelStyle: TextStyle(color: Colors.white, fontSize: 18)
+            labelStyle: const TextStyle(color: Colors.white, fontSize: 18)
           ),
         ),),
       );
@@ -104,19 +104,19 @@ class _Mentor1State extends State<Mentor1> {
         margin: EdgeInsets.only(left: leftMargin, right:rightMargin, top:20),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
-          gradient: LinearGradient(colors: [Color(0xFF10FFA9).withOpacity(0.2), Color(0XFFFFFFFF).withOpacity(0.2)]),
+          gradient: LinearGradient(colors: [const Color(0xFF10FFA9).withOpacity(0.2), const Color(0XFFFFFFFF).withOpacity(0.2)]),
         ),
         child: SizedBox(
           width: 150,
           child: TextField(
-            style: TextStyle(color: Colors.white, fontSize: 18),
+            style: const TextStyle(color: Colors.white, fontSize: 18),
           decoration: InputDecoration(
             border: OutlineInputBorder(
               borderSide: BorderSide.none,
               borderRadius: BorderRadius.circular(20)
             ),
             labelText: label,
-            labelStyle: TextStyle(color: Colors.white, fontSize: 18)
+            labelStyle: const TextStyle(color: Colors.white, fontSize: 18)
           ),
         ),),
       );
