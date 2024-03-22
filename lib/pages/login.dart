c33650a9501157d012f20e305e4979c67348d6fd
RZRexton <ooiruizhe@gmail.com>
2024-03-23 02:50:28 +0800

    Updated Loading

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,5 @@
+import 'package:fivenance/pages/home.dart';
+import 'package:fivenance/pages/profile.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -6,166 +8,256 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF0C1C3C),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(top: 100),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 20, left: 25, right: 20),
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Transform.scale(
-                        scale: 0.8, // Adjust the scale factor as needed
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.white,
+  bool _isLoading = false;
+
+  void _showSaveConfirmation(BuildContext context) {
+    setState(() {
+      _isLoading = true; // Set loading state to true
+    });
+
+    // Simulate delay before showing Save.png
+    Future.delayed(Duration(seconds: 2), () {
+      // Close loading dialog
+      setState(() {
+        _isLoading = false;
+      });
+
+      // Show the Save.png image and back button
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 20),
+                Material(
+                  color: Colors.transparent,
+                  child: Image.asset(
+                    'assets/LoginSuccess.png',
+                    width: 325,
+                    height: 234,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 50,
+                  width: 170,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 54, 199, 117)),
+                      shape: MaterialStateProperty.all<OutlinedBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: BorderSide(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                    SizedBox(width: 120),
-                    Spacer(),
-                  ],
+                    child: Text(
+                      'OK',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+              ],
+            ),
+          );
+        },
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: Color(0xFF0C1C3C),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(top: 100),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Column(
+                  Padding(
+                    padding: EdgeInsets.only(top: 20, left: 25, right: 20),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Transform.scale(
+                            scale: 0.8, // Adjust the scale factor as needed
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 120),
+                        Spacer(),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
-                      Text('Login',
-                          style: TextStyle(color: Colors.white, fontSize: 46)),
-                      Text('Glad you\'re here!',
-                          style: TextStyle(
-                              color: Color(0xFFCACACA), fontSize: 16)),
+                      Column(
+                        children: <Widget>[
+                          Text('Login',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 46)),
+                          Text('Glad you\'re here!',
+                              style: TextStyle(
+                                  color: Color(0xFFCACACA), fontSize: 16)),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(40),
+                        alignment: Alignment.center,
+                        child: Image(
+                          width: 140,
+                          image: AssetImage('assets/Logo.png'),
+                        ),
+                      ),
                     ],
                   ),
                   Container(
-                    margin: EdgeInsets.all(40),
-                    alignment: Alignment.center,
-                    child: Image(
-                      width: 140,
-                      image: AssetImage('assets/Logo.png'),
+                    margin: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(colors: [
+                        Color(0xFF10FFA9).withOpacity(0.2),
+                        Color(0XFFFFFFFF).withOpacity(0.2)
+                      ]),
+                    ),
+                    child: SizedBox(
+                      width: 250,
+                      child: TextField(
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              labelText: 'Username',
+                              labelStyle: TextStyle(
+                                  color: Colors.white, fontSize: 18))),
                     ),
                   ),
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(colors: [
-                    Color(0xFF10FFA9).withOpacity(0.2),
-                    Color(0XFFFFFFFF).withOpacity(0.2)
-                  ]),
-                ),
-                child: SizedBox(
-                  width: 250,
-                  child: TextField(
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          labelText: 'Username',
-                          labelStyle:
-                              TextStyle(color: Colors.white, fontSize: 18))),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(colors: [
-                    Color(0xFF10FFA9).withOpacity(0.2),
-                    Color(0XFFFFFFFF).withOpacity(0.2)
-                  ]),
-                ),
-                child: SizedBox(
-                  width: 250,
-                  child: TextField(
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          labelText: 'Password',
-                          labelStyle:
-                              TextStyle(color: Colors.white, fontSize: 18))),
-                ),
-              ),
-              Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [Color(0xFFD3FFF9), Color(0XFF61FF89)]),
-                      borderRadius: BorderRadius.all(Radius.circular(30))),
-                  margin: EdgeInsets.only(left: 20, top: 20, right: 20),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                          '/home', (Route<dynamic> route) => false);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent),
-                    child: Text('Login',
-                        style: TextStyle(
-                            fontFamily: 'Judson',
-                            color: Colors.black,
-                            fontSize: 23)),
-                  )),
-              TextButton(
-                  onPressed: null,
-                  child: Text('Forgot Password?',
-                      style: TextStyle(color: Colors.white, fontSize: 16))),
-              Image(
-                image: AssetImage('assets/Or.png'),
-              ),
-              Container(
-                  margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-                  child: const ElevatedButton(
-                    onPressed: null,
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll(Colors.white)),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Image(
-                            image: AssetImage('assets/Google1.png'),
-                          ),
-                          Text('   Log in with Google',
-                              style: TextStyle(
-                                  fontFamily: 'Judson',
-                                  color: Colors.black,
-                                  fontSize: 19)),
-                        ]),
-                  )),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('DON\'T HAVE AN ACCOUNT? ',
-                      style: TextStyle(color: Color(0xFFB6B6B6), fontSize: 13)),
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(colors: [
+                        Color(0xFF10FFA9).withOpacity(0.2),
+                        Color(0XFFFFFFFF).withOpacity(0.2)
+                      ]),
+                    ),
+                    child: SizedBox(
+                      width: 250,
+                      child: TextField(
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                  color: Colors.white, fontSize: 18))),
+                    ),
+                  ),
+                  Container(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Color(0xFFD3FFF9), Color(0XFF61FF89)]),
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      margin: EdgeInsets.only(left: 20, top: 20, right: 20),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _showSaveConfirmation(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            shadowColor: Colors.transparent),
+                        child: Text('Login',
+                            style: TextStyle(
+                                fontFamily: 'Judson',
+                                color: Colors.black,
+                                fontSize: 23)),
+                      )),
                   TextButton(
                       onPressed: null,
-                      child: Text('SIGN UP',
-                          style: TextStyle(color: Colors.white, fontSize: 13)))
+                      child: Text('Forgot Password?',
+                          style: TextStyle(color: Colors.white, fontSize: 16))),
+                  Image(
+                    image: AssetImage('assets/Or.png'),
+                  ),
+                  Container(
+                      margin:
+                          const EdgeInsets.only(left: 20, right: 20, top: 20),
+                      child: const ElevatedButton(
+                        onPressed: null,
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStatePropertyAll(Colors.white)),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Image(
+                                image: AssetImage('assets/Google1.png'),
+                              ),
+                              Text('   Log in with Google',
+                                  style: TextStyle(
+                                      fontFamily: 'Judson',
+                                      color: Colors.black,
+                                      fontSize: 19)),
+                            ]),
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('DON\'T HAVE AN ACCOUNT? ',
+                          style: TextStyle(
+                              color: Color(0xFFB6B6B6), fontSize: 13)),
+                      TextButton(
+                          onPressed: null,
+                          child: Text('SIGN UP',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 13)))
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         ),
-      ),
+        // Overlay for loading indicator
+        if (_isLoading)
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: HourGlassLoadingWidget(),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
--- lib/pages/mentor2.dart
+++ lib/pages/mentor2.dart
@@ -6,76 +6,112 @@ class Mentor2 extends StatefulWidget {
 }
 
 String? dropdownValue;
-class _Mentor2State extends State<Mentor2> {
 
+class _Mentor2State extends State<Mentor2> {
   bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0C1C3C),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                const Column(children: <Widget>[
-                  Text('Mentorship', style: TextStyle(color: Colors.white, fontSize: 40)),
-                  Text('Sign Up', style: TextStyle(color: Color(0xFFCACACA), fontSize: 40)),
-                ],),
+        backgroundColor: const Color(0xFF0C1C3C),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    const Column(
+                      children: <Widget>[
+                        Text('Mentorship',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 40)),
+                        Text('Sign Up',
+                            style: TextStyle(
+                                color: Color(0xFFCACACA), fontSize: 40)),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(40),
+                      alignment: Alignment.center,
+                      child: const Image(
+                        width: 100,
+                        image: AssetImage('assets/Logo.png'),
+                      ),
+                    ),
+                  ],
+                ),
                 Container(
-                margin: const EdgeInsets.all(40),
-                alignment: Alignment.center,
-                child: const Image(
-                  width: 100,
-                  image: AssetImage('assets/Logo.png'),
-                ),),
-              ],),
-
-            Container(
-              margin:const EdgeInsets.only(left:20, right:20, bottom: 20),
-              child: const LinearProgressIndicator(value: 0.66, color: Color(0xFF64FF8C), backgroundColor: Color(0xFF523F62),)),
-
-            const Center(child: Text('Page 2: Relevant Qualifications', style: TextStyle(color: Colors.white, fontSize: 26))),
-            Container(margin: const EdgeInsets.only(left: 20, top: 10), child: const Text('Certification*', style: TextStyle(color: Colors.white, fontSize: 15))),
-            Container(margin: const EdgeInsets.all(20),child: const Image(image: AssetImage('assets/Upload2nd.png'),)),
-            Container(margin: const EdgeInsets.only(left:20), child: const Text('Please upload any relevant certificates  that demonstrate your expertise, experience, or completion of training programs to  support your qualifications for mentoring.', style: TextStyle(color: Colors.white, fontSize: 13))),
-            Container(
-              margin: const EdgeInsets.all(20),
-              child: const TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'Other:',
-                  labelStyle: TextStyle(color: Colors.white, fontSize: 15),
-                  hintText: 'Other qualifications or training in word form...',
-                  hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
+                    margin:
+                        const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                    child: const LinearProgressIndicator(
+                      value: 0.66,
+                      color: Color(0xFF64FF8C),
+                      backgroundColor: Color(0xFF523F62),
+                    )),
+                const Center(
+                    child: Text('Page 2: Relevant Qualifications',
+                        style: TextStyle(color: Colors.white, fontSize: 26))),
+                Container(
+                    margin: const EdgeInsets.only(left: 20, top: 10),
+                    child: const Text('Certification*',
+                        style: TextStyle(color: Colors.white, fontSize: 15))),
+                Container(
+                    margin: const EdgeInsets.all(20),
+                    child: const Image(
+                      image: AssetImage('assets/Upload2nd.png'),
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    child: const Text(
+                        'Please upload any relevant certificates  that demonstrate your expertise, experience, or completion of training programs to  support your qualifications for mentoring.',
+                        style: TextStyle(color: Colors.white, fontSize: 13))),
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  child: const TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: 'Other:',
+                        labelStyle:
+                            TextStyle(color: Colors.white, fontSize: 15),
+                        hintText:
+                            'Other qualifications or training in word form...',
+                        hintStyle: TextStyle(color: Colors.grey, fontSize: 14),
+                      )),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      OutlinedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: OutlinedButton.styleFrom(
+                            side: const BorderSide(color: Color(0xFF6DFF95))),
+                        child: const Text('Back',
+                            style: TextStyle(
+                                fontFamily: 'Judson',
+                                color: Color(0xFF6DFF95),
+                                fontSize: 16)),
+                      ),
+                      const ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStatePropertyAll(Color(0xFF55E985))),
+                          onPressed: null,
+                          child: Text('Next',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16)))
+                    ],
+                  ),
                 )
-              ),
+              ],
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                OutlinedButton(
-                  onPressed: (){
-                    Navigator.pop(context);
-                  },
-                  style: OutlinedButton.styleFrom(side: const BorderSide(color: Color(0xFF6DFF95))),
-                  child: const Text('Back', style: TextStyle(fontFamily: 'Judson', color: Color(0xFF6DFF95), fontSize: 16)),),
-              
-                const ElevatedButton(
-                  style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color(0xFF55E985))),
-                  onPressed: null, child: Text('Next', style: TextStyle(color: Colors.black, fontSize: 16)))
-              ],),
-            )
-          ],),
-        ),
-      )
-    );
+          ),
+        ));
   }
 }
